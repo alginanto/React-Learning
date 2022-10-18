9ad894e6f5db0c3f0034718a3be6e09e393863d5
Algin Anto <alginanto143@gmail.com>
2022-10-18 18:35:58 +0530

    tic tac toe latest update

--- src/Components/Square/Square.jsx
+++ src/Components/Square/Square.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import './square.css';
 
-export class Square extends React.Component {
+// export class Square extends React.Component {
 
-    render() {
-      return (
-        <button 
-        className="square" 
-        onClick={()=>{this.props.onClick()}}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+//     render() {
+//       return (
+//         <button 
+//         className="square" 
+//         onClick={()=>{this.props.onClick()}}
+//         >
+//           {this.props.value}
+//         </button>
+//       );
+//     }`
+//   }
+export function Square(props){
+    return (
+                <button 
+                className="square" 
+                onClick={()=>{props.onClick()}}
+                >
+                  {props.value}
+                </button>
+              );
+    
+}
